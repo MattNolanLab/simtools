ee6c87902fcf8a0ce260ce62e56d04a22d9fe861
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-05-07 16:02:46 +0100

    add flagparse module and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='pysimlets',
+        version='0.0.0',
+        description='Python simulation and analysis helpers.',
+        author='Lukas Solanka',
+        author_email='lsolanka@gmail.com',
+        packages=['pysimlets']
+)
+
