89526608dd60ed6a10045a9e5744840721bf59cc
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-12-14 22:11:12 +0000

    set up tox.ini and travis

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
+'''Setup script for simtools.'''
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(
-        name='simtools',
-        version='0.1.0',
-        description='Python simulation and analysis helpers.',
-        author='Lukas Solanka',
-        author_email='lsolanka@gmail.com',
-        packages=['simtools']
-)
+setup(name='simtools',
+      version='0.1.0',
+      description='Python simulation and analysis helpers.',
+      author='Lukas Solanka',
+      author_email='lsolanka@gmail.com',
+      packages=['simtools'])
 
