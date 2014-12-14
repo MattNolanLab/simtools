d451cd25b33be083c152a266f8eed17761de11c3
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-12-14 18:58:43 +0000

    refactor flagparse (is now arguments)
    FlagParser stays the same.
    New classes:
    
      - SimulationParser - only sets the verbosity level
    
      - FlagRunner - more appropriate name for FlagParser (only an alias)
    
    Add tests for the arguments module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='pysimlets',
-        version='0.0.0',
+        version='0.1.0',
         description='Python simulation and analysis helpers.',
         author='Lukas Solanka',
         author_email='lsolanka@gmail.com',
