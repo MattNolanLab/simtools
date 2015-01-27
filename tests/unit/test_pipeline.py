91e250488409fab0a970766a269ad60fc2ffdf60
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-01-27 17:10:46 +0000

    add very basic classes for pipeline processing.

---
+++ tests/unit/test_pipeline.py
@@ -0,0 +1,41 @@
+'''Test the pipeline module.'''
+from __future__ import absolute_import, print_function, division
+
+from simtools.pipelines import PipelineStage, Pipeline, PipelineData
+
+
+class Increment(PipelineStage):
+    '''A simple pipeline stage that prints a number and increments by one.'''
+    def __call__(self, data_in):
+        print(data_in.items['num'])
+        data_in.items['num'] += 1
+        return data_in
+
+
+class TestPipeline(object):
+    '''Test the pipeline class.'''
+
+    def _test(self, num):
+        '''Insert ``num`` simple stages into the pipeline and run it.'''
+        pipeline = Pipeline()
+        assert pipeline.num_stages == 0
+
+        for n_stage in range(num):
+            increment_stage = Increment()
+            pipeline.append(increment_stage)
+            assert pipeline.num_stages == n_stage + 1
+
+        data = PipelineData()
+        data.items['num'] = 0
+        data = pipeline.run(data)
+
+        assert data.items['num'] == num
+
+    def test_increment(self):
+        '''Test a simple increment procedure.'''
+        # Empty pipeline should run but do nothing.
+        self._test(0)
+        # One, two, ten
+        self._test(1)
+        self._test(2)
+        self._test(10)
